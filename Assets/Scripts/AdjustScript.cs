46bb73367459dd364032f743af308988dca210a9
n01073 <n01073@cybird-51391.intra.cybird.co.jp>
2016-08-08 14:28:26 +0900

    Save and Load is OK

--- Assets/Scripts/AdjustScript.cs
+++ Assets/Scripts/AdjustScript.cs
@@ -21,5 +21,14 @@ public class AdjustScript : MonoBehaviour {
 		{
 			GameControl.control.experience -= 10;
 		}
+
+		if (GUI.Button (new Rect (10, 260, 100, 30), "Save")) 
+		{
+			GameControl.control.Save ();
+		}
+		if (GUI.Button (new Rect (10, 300, 100, 30), "Load")) 
+		{
+			GameControl.control.Load ();
+		}
 	}
 }
--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class GameControl : MonoBehaviour {
 
@@ -32,4 +35,38 @@ public class GameControl : MonoBehaviour {
 	public void loadScene(string sceneName) {
 		SceneManager.LoadScene (sceneName);
 	}
+
+	public void Save() {
+
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (Application.persistentDataPath + "/playerInfo.dat");
+
+		PlayerData data = new PlayerData ();
+
+		data.health = health;
+		data.experience = experience;
+
+		bf.Serialize (file, data);
+		file.Close ();
+	}
+
+	public void Load() {
+
+		if (File.Exists (Application.persistentDataPath + "/playerInfo.dat")) {
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open (Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+			PlayerData data = (PlayerData) bf.Deserialize (file);
+			file.Close ();
+
+			health = data.health;
+			experience = data.experience;
+		}
+	}
+}
+
+[Serializable]
+class PlayerData
+{
+	public float health;
+	public float experience;
 }
